96c00ba1d3150ecaf5181118540dc65deef2c6d8
sergycg <sergycg@gmail.com>
2021-07-19 19:24:03 +0200

    se añade cron para que no se duerma en heroku

--- src/main/java/com/recetas/RecetasCocinaApplication.java
+++ src/main/java/com/recetas/RecetasCocinaApplication.java
@@ -1,22 +1,38 @@
 package com.recetas;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableScheduling
 public class RecetasCocinaApplication {
+
+	@Value("${herokuNotIdle.url}")
+	private String herokuNotIdle_url;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecetasCocinaApplication.class, args);
 	}
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
+	@Scheduled(cron = "${herokuNotIdle.cronExpression}")
+	public void herokuNotIdle(RestTemplate restTemplate) {
+		System.out.println(" INIT - Heroku not idle execution ");
+		ResponseEntity<String> response = restTemplate.exchange(herokuNotIdle_url, HttpMethod.GET, null, String.class);
+		System.out.println(response);
+		System.out.println(" FINISH - Heroku not idle execution");
+	}
+}
